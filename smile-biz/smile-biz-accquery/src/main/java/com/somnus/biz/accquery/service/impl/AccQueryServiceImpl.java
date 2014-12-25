3c79f9d0d62a10bfe162bfa0a2769ea97daf6cb1
love-somnus <928200207@qq.com>
2014-12-26 00:01:26 +0800

    version1.1.1

--- smile-biz/smile-biz-accquery/src/main/java/com/somnus/biz/accquery/service/impl/AccQueryServiceImpl.java
+++ smile-biz/smile-biz-accquery/src/main/java/com/somnus/biz/accquery/service/impl/AccQueryServiceImpl.java
@@ -25,19 +25,19 @@ public class AccQueryServiceImpl implements AccQueryService {
 
 	@Override
 	public QueryPayAppResponse queryPaymentApp(QueryPayAppRequest request) throws Exception {
-		/*if(request == null) {
+		if(request == null) {
 			throw new BizException(msa.getMessage(MsgCodeList.ERROR_302001, new Object[]{"QueryPayAppRequest"}));
 		}
 		PageList<QueryPayAppOrder> pagelist = paymentAppDao.selectPayAppOrders(request,new PageBounds(request.getPageNum(),request.getPageSize()));
 		int total = 0;//总记录数
 		if(pagelist != null && !pagelist.isEmpty()) {
 			total = pagelist.getPaginator().getTotalCount();
-		}*/
+		}
 		QueryPayAppResponse response = new QueryPayAppResponse();
-		/*response.setPageNum(request.getPageNum());
+		response.setPageNum(request.getPageNum());
 		response.setPageSize(request.getPageSize());
 		response.setRowCount(total);
-		response.setOrders(pagelist);*/
+		response.setOrders(pagelist);
 		return response;
 	}
 }
