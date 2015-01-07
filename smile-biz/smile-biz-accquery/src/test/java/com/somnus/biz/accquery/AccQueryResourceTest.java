765edef778251a9e1f78dd92525f3f3f26720e1f
love-somnus <928200207@qq.com>
2015-01-07 18:24:15 +0800

    version1.1.1

--- smile-biz/smile-biz-accquery/src/test/java/com/somnus/biz/accquery/AccQueryResourceTest.java
+++ smile-biz/smile-biz-accquery/src/test/java/com/somnus/biz/accquery/AccQueryResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import com.somnus.biz.accquery.resource.AccQueryResource;
 import com.somnus.message.accquery.QueryPayAppRequest;
 import com.somnus.message.accquery.QueryPayAppResponse;
+import com.somnus.rest.core.client.RESTfulJsonClientFactory;
 import com.somnus.support.holder.ApplicationContextHolder;
 import com.somnus.support.util.JsonUtils;
 
@@ -13,7 +14,24 @@ import com.somnus.support.util.JsonUtils;
  *
  */
 public class AccQueryResourceTest extends AbstractTestSupport{
-    
+	private final static String BASE_URL = "http://localhost:8080/smile/resource/";
+	AccQueryResource accQueryResource = RESTfulJsonClientFactory.createClient(AccQueryResource.class, BASE_URL);
+	
+	@Test
+    public void queryPaymentAppTestcaseByRestful() throws Exception {
+    	QueryPayAppRequest request = new QueryPayAppRequest();
+    	request.setSysCode("test");
+    	request.setBrcCode("test");
+    	request.setChannelNo("test");
+    	request.setTerminalNo("test");
+    	request.setFrontNo("test");
+    	request.setFrontTime("test");
+    	request.setPageNum(2);
+    	request.setPageSize(10);
+    	QueryPayAppResponse response = accQueryResource.queryPaymentApp(request);
+    	System.out.println("响应结果>>>>>>>>>>>>> : " + JsonUtils.toString(response));
+    }
+	
     @Test
     public void queryPaymentAppTestcase() throws Exception {
     	QueryPayAppRequest request = new QueryPayAppRequest();
